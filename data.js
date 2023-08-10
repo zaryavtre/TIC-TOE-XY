d64ce8e2cc366dd42cd7eab1ab772e2e6be0acd1
zaryavtre <von.zaryavtre@gmail.com>
2023-08-10 01:10:42 +0100

    log 003

--- data.js
+++ data.js
@@ -1,19 +1,35 @@
 const mainApp = document.querySelector('.main')
 
-function gameBoard() {
+function mainGrid() {
   const canvas = document.createElement('grid')
-  const boardHeading = document.createElement('h1')
-  const heading = document.createTextNode('Welcome to Tic Tac Toe X')
-  boardHeading.appendChild(heading)
   canvas.style.display = 'grid'
   canvas.style.width = '100%'
   canvas.style.height = '100%'
   canvas.style.gridTemplateColumns = 'repeat(3, 1fr)'
   canvas.style.gridTemplateRows = 'repeat(3, 1fr)'
   canvas.classList.add('canvas')
-  mainApp.appendChild(boardHeading)
+
   mainApp.appendChild(canvas)
+
+  for(let i = 0; i < 3 * 3; i++) {
+    let increment = 0
+    const gridElement = document.createElement('div')
+    gridElement.style.border = 'solid 1px black'
+    gridElement.classList.add(`${increment += 1}`)
+    canvas.appendChild(gridElement)
+  }
 }
+
+function gameBoard() {
+  mainGrid()
+  const boardHeading = document.createElement('h1')
+  const heading = document.createTextNode('Welcome to Tic Tac Toe X')
+  boardHeading.appendChild(heading)
+  mainApp.appendChild(boardHeading)
+
+}
+
+
 
 const playersFactory = (player1, player2) => {}
 
