89ab591dc376344d7d925507c361df3c0101739d
zaryavtre <von.zaryavtre@gmail.com>
2023-08-14 01:43:11 +0100

    log 006

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 import { gameBoard } from './board.js'
 import { player1, player2 } from './players.js'
 
-gameBoard()
+const players = [{
+  name: player1,
+  token: circle
+}, {
+  name: player2,
+  token: cross
+}]
+
+function playRound() {
+  let activePlayer = players[0]
+
+  const switchPlayer = () => {
+    activePlayer = activePlayer === players[0] ? players[0] : players[1]
+  }
+}
 
 document.addEventListener('click', function (e) {
   const clickedId = e.target.id
@@ -9,7 +23,7 @@ document.addEventListener('click', function (e) {
   const clickedElement = document.getElementById(clickedId)
   if(!e.target.dataset.taken) {
     if(clickedId && clickedElement) {
-      player2(clickedElement)
+      
     }
   }
 })
